f7d60c0af1b38290ad5411c98fc79110d3f015fc
Ldar72 <l-dar@mail.ru>
2024-07-30 14:43:43 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,4 +1,3 @@
-dict_grades_students = {}
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 a = list(students)
@@ -6,7 +5,5 @@ students = (sorted(a))
 grades_medium = (sum(grades[0]) / len(grades[0]), sum(grades[1]) / len(grades[1]),
                  sum(grades[2]) / len(grades[2]), sum(grades[3]) / len(grades[3]),
                  sum(grades[4]) / len(grades[4]))
-dict_grades_students = {(students[0]): (grades_medium[0]), (students[1]): (grades_medium[1]),
-                        (students[2]): (grades_medium[2]), (students[3]): (grades_medium[3]),
-                        (students[4]): (grades_medium[4])}
-print(dict_grades_students)
+students_and_grades_medium = dict(zip(students, grades_medium))
+print(students_and_grades_medium)
